87fdd2c92748f0143e4e35d25d8abe05166adeab
Martin Kortkamp <martin.kortkamp@gmx.de>
2016-04-30 11:09:22 +0200

    fix lzma unpacking by fixing seeking from streamend

--- src/main/com/mucommander/commons/file/impl/sevenzip/SignatureCheckedRandomAccessFile.java
+++ src/main/com/mucommander/commons/file/impl/sevenzip/SignatureCheckedRandomAccessFile.java
@@ -20,6 +20,7 @@ package com.mucommander.commons.file.impl.sevenzip;
 import com.mucommander.commons.file.AbstractFile;
 import com.mucommander.commons.file.FileOperation;
 import com.mucommander.commons.file.UnsupportedFileOperationException;
+import com.mucommander.commons.io.RandomAccessInputStream;
 import com.mucommander.commons.io.StreamUtils;
 import net.sf.sevenzipjbinding.IInStream;
 import net.sf.sevenzipjbinding.ISequentialInStream;
@@ -27,13 +28,12 @@ import net.sf.sevenzipjbinding.SevenZipException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.trolsoft.utils.StrUtils;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 
 /**
- * @author  Oleg Trifonov
+ * @author Oleg Trifonov
  */
 public class SignatureCheckedRandomAccessFile implements IInStream, ISequentialInStream {
 
@@ -62,23 +62,84 @@ public class SignatureCheckedRandomAccessFile implements IInStream, ISequentialI
         }
     }
 
-
     @Override
     public long seek(long offset, int seekOrigin) throws SevenZipException {
         try {
-            if (seekOrigin == SEEK_SET) {
-                stream.close();
-                stream = file.getInputStream();
-                stream.skip(offset);
-                position = offset;
-            } else if (seekOrigin == SEEK_CUR) {
-                stream.skip(offset);
-                position += offset;
+            if (file.isFileOperationSupported(FileOperation.RANDOM_READ_FILE)) {
+                seekOnRandomAccessFile(offset, seekOrigin);
+            } else {
+                seekOnSequentialFile(offset, seekOrigin);
             }
         } catch (IOException e) {
             throw new SevenZipException(e);
         }
         return position;
+    }
+
+    private void seekOnRandomAccessFile(long offset, int seekOrigin) throws IOException {
+        RandomAccessInputStream randomAccessInputStream = (RandomAccessInputStream) stream;
+        switch (seekOrigin) {
+        case SEEK_SET:
+            position = offset;
+            break;
+        case SEEK_CUR:
+            position += offset;
+            break;
+        case SEEK_END:
+            position = randomAccessInputStream.getLength() + offset;
+            break;
+        }
+        randomAccessInputStream.seek(position);
+    }
+
+    /**
+     * @param offset
+     * @param seekOrigin
+     * @throws IOException
+     */
+    private void seekOnSequentialFile(long offset, int seekOrigin) throws IOException {
+        switch (seekOrigin) {
+        case SEEK_SET:
+            stream.close();
+            stream = file.getInputStream();
+            skip(offset);
+            position = offset;
+            break;
+        case SEEK_CUR:
+            skip(offset);
+            position += offset;
+            break;
+        case SEEK_END:
+            long size = file.getSize();
+            if (size == -1) {
+                skip(offset);
+                size = position;
+            }
+            position = size + offset;
+            stream.close();
+            stream = file.getInputStream();
+            stream.skip(position);
+            break;
+        }
+    }
+
+    /**
+     * @param skip
+     * @throws IOException
+     */
+    private void skip(long skip) throws IOException {
+        long skipped = stream.skip(skip);
+        position += skipped;
+        while (skipped < skip) {
+            byte[] skipBuffer = new byte[1024];
+            int read = stream.read(skipBuffer, 0, skipBuffer.length);
+            if (read == -1) {
+                break;
+            } else {
+                position += read;
+                skipped += read;
+            }
+        }
     }
 
     @Override
@@ -93,22 +154,35 @@ public class SignatureCheckedRandomAccessFile implements IInStream, ISequentialI
         }
     }
 
-
     private InputStream openStreamAndCheckSignature(AbstractFile file) throws IOException {
-        byte[] buf = new byte[16];
-        PushbackInputStream pushbackInputStream = file.getPushBackInputStream(buf.length);
-        int read = StreamUtils.readUpTo(pushbackInputStream, buf);
-        // TODO sometimes reading from pushbackInputStream returns 0
-        if (read <= 0 && file.getSize() > 0) {
-            return file.getInputStream();
+        byte[] buf = new byte[signature.length];
+
+        InputStream iStream;
+
+        int read;
+        if (file.isFileOperationSupported(FileOperation.RANDOM_READ_FILE)) {
+            RandomAccessInputStream raiStream = file.getRandomAccessInputStream();
+            raiStream.seek(0);
+            read = StreamUtils.readUpTo(raiStream, buf);
+            raiStream.seek(0);
+            iStream = raiStream;
+        } else {
+            PushbackInputStream pushbackInputStream = file.getPushBackInputStream(buf.length);
+            iStream = pushbackInputStream;
+            read = StreamUtils.readUpTo(pushbackInputStream, buf);
+            // TODO sometimes reading from pushbackInputStream returns 0
+            if (read <= 0 && file.getSize() > 0) {
+                return file.getInputStream();
+            }
+            pushbackInputStream.unread(buf, 0, read);
         }
-        pushbackInputStream.unread(buf, 0, read);
+
         if (!checkSignature(buf)) {
-            pushbackInputStream.close();
+            iStream.close();
             throw new IOException("Wrong file signature was " + StrUtils.bytesToHexStr(buf, 0, read)
                     + " but should be " + StrUtils.bytesToHexStr(signature, 0, signature.length));
         }
-        return pushbackInputStream;
+        return iStream;
     }
 
     private boolean checkSignature(byte[] data) {
