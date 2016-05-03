a853c69151ec7153f251d4f354a6deb96b4f6aed
Martin Kortkamp <martin.kortkamp@gmx.de>
2016-05-03 23:53:56 +0200

    7z fix randomaccess IOutStream is needed for 7z format

--- src/main/com/mucommander/commons/file/archiver/SevenZipArchiver.java
+++ src/main/com/mucommander/commons/file/archiver/SevenZipArchiver.java
@@ -1,21 +1,28 @@
 package com.mucommander.commons.file.archiver;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import com.mucommander.commons.file.FileAttributes;
+import com.mucommander.commons.file.impl.local.LocalFile;
+import com.mucommander.commons.io.RandomAccessOutputStream;
+import com.mucommander.commons.io.StreamUtils;
 import com.mucommander.utils.ThrowingConsumer;
 import net.sf.sevenzipjbinding.IOutCreateArchive7z;
 import net.sf.sevenzipjbinding.IOutCreateCallback;
 import net.sf.sevenzipjbinding.IOutItem7z;
+import net.sf.sevenzipjbinding.IOutStream;
 import net.sf.sevenzipjbinding.ISequentialInStream;
-import net.sf.sevenzipjbinding.ISequentialOutStream;
 import net.sf.sevenzipjbinding.SevenZip;
 import net.sf.sevenzipjbinding.SevenZipException;
 import net.sf.sevenzipjbinding.impl.InputStreamSequentialInStream;
@@ -26,13 +33,27 @@ public class SevenZipArchiver extends Archiver {
     private final IOutCreateArchive7z outArchive7z;
     private final List<CreateEntryAsyncParameter> entries = new ArrayList<>();
     private ExecutorService executor = Executors.newCachedThreadPool(); // TODO share this executor
+    private Path tempFile;
+    private OutputStream sequentialOut;
 
     SevenZipArchiver(OutputStream out) throws IOException {
-        super(out);
+        super(null);
+        this.out = wrapSequentialOutputStream(out);
         try {
             outArchive7z = SevenZip.openOutArchive7z();
         } catch (SevenZipException e) {
             throw new IOException(e);
+        }
+    }
+
+    private RandomAccessOutputStream wrapSequentialOutputStream(OutputStream outStream) throws IOException {
+        if (outStream instanceof RandomAccessOutputStream) {
+            return (RandomAccessOutputStream) outStream;
+        } else {
+            tempFile = Files.createTempFile("trolCommander-packer", "7z");
+            tempFile.toFile().deleteOnExit();
+            sequentialOut = outStream;
+            return new LocalFile.LocalRandomAccessOutputStream(FileChannel.open(tempFile));
         }
     }
 
@@ -41,7 +62,7 @@ public class SevenZipArchiver extends Archiver {
         try {
             outArchive7z.setLevel(9);
             outArchive7z.setTrace(true);
-            outArchive7z.createArchive(new ISequentialOutStream() {
+            outArchive7z.createArchive(new IOutStream() {
 
                 @Override
                 public int write(byte[] bytes) throws SevenZipException {
@@ -51,6 +72,42 @@ public class SevenZipArchiver extends Archiver {
                         throw new SevenZipException(e);
                     }
                     return bytes.length;
+                }
+
+                @Override
+                public long seek(long offset, int seekOrigin) throws SevenZipException {
+                    final RandomAccessOutputStream raOut = (RandomAccessOutputStream) out;
+                    long offsetBeginning;
+                    try {
+                        switch (seekOrigin) {
+                            case SEEK_SET:
+                                offsetBeginning = offset;
+                                break;
+                            case SEEK_CUR:
+                                offsetBeginning = raOut.getOffset() + offset;
+                                break;
+                            case SEEK_END:
+                                offsetBeginning = raOut.getLength() + offset;
+                                break;
+                            default:
+                                throw new SevenZipException(
+                                        new IllegalArgumentException("invalid seekOrigin: " + seekOrigin));
+                        }
+                        raOut.seek(offsetBeginning);
+                        return raOut.getOffset();
+                    } catch (IOException e) {
+                        throw new SevenZipException(e);
+                    }
+                }
+
+                @Override
+                public void setSize(long newSize) throws SevenZipException {
+                    final RandomAccessOutputStream raOut = (RandomAccessOutputStream) out;
+                    try {
+                        raOut.setLength(newSize);
+                    } catch (IOException e) {
+                        throw new SevenZipException(e);
+                    }
                 }
             },
                     entries.size(),
@@ -150,7 +207,10 @@ public class SevenZipArchiver extends Archiver {
 
     @Override
     public void postProcess() throws IOException {
-
+        if (tempFile != null) {
+            StreamUtils.copyStream(new FileInputStream(tempFile.toFile()), sequentialOut);
+            tempFile.toFile().delete();
+        }
     }
 
     @Override
